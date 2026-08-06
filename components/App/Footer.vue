e7e3352d34c200f3d7df0ddb7396c4b78cca1b98
Hüseyin ONUR <huseyin.onr60@gmail.com>
2026-08-06 12:24:18 +0000

    project reniew

--- components/App/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer
-    class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8"
-  >
-    <br>
-    <p>© 2024 Huseyin Onur. All rights reserved.</p>
-  </footer>
-</template>
